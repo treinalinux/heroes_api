63de901fea5fd26191abd3c2534f75ae9cfa747f
Alan da Silva Alves <linuxfama@gmail.com>
2021-09-26 08:35:20 -0300

    feat: seed with add faker group development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hero.delete_all
+
+10.times do
+  Hero.create name: Faker::Superhero.name
+end
