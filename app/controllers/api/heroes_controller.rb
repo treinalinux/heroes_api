a25f6e0357ef3cd1d8662073cb2e12d59f2e07b1
Alan da Silva Alves <linuxfama@gmail.com>
2021-09-27 00:21:24 -0300

    fix: bug fix location on create

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -1,55 +1,57 @@
 # frozen_string_literal: true
 
-# class HeroesController
-class Api::HeroesController < ApplicationController
-  before_action :set_hero, only: %i[show update destroy]
+module Api
+  # class HeroesController
+  class HeroesController < ApplicationController
+    before_action :set_hero, only: %i[show update destroy]
 
-  # GET /heroes
-  def index
-    @heroes = Hero.all
+    # GET /heroes
+    def index
+      @heroes = Hero.all
 
-    render json: @heroes
-  end
-
-  # GET /heroes/1
-  def show
-    render json: @hero
-  end
-
-  # POST /heroes
-  def create
-    @hero = Hero.new(hero_params)
-
-    if @hero.save
-      render json: @hero, status: :created, location: @hero
-    else
-      render json: @hero.errors, status: :unprocessable_entity
+      render json: @heroes
     end
-  end
 
-  # PATCH/PUT /heroes/1
-  def update
-    if @hero.update(hero_params)
+    # GET /heroes/1
+    def show
       render json: @hero
-    else
-      render json: @hero.errors, status: :unprocessable_entity
     end
-  end
 
-  # DELETE /heroes/1
-  def destroy
-    @hero.destroy
-  end
+    # POST /heroes
+    def create
+      @hero = Hero.new(hero_params)
 
-  private
+      if @hero.save
+        render json: @hero, status: :created, location: api_hero_url(@hero)
+      else
+        render json: @hero.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_hero
-    @hero = Hero.find(params[:id])
-  end
+    # PATCH/PUT /heroes/1
+    def update
+      if @hero.update(hero_params)
+        render json: @hero
+      else
+        render json: @hero.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Only allow a list of trusted parameters through.
-  def hero_params
-    params.require(:hero).permit(:name)
+    # DELETE /heroes/1
+    def destroy
+      @hero.destroy
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_hero
+      @hero = Hero.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def hero_params
+      params.require(:hero).permit(:name)
+    end
   end
 end
